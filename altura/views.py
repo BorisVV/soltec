129819116b1640c84a1ec6b247ae18bf9f2b899c
BorisVV <bcarlof@gmail.com>
2018-08-05 13:01:47 -0500

    add pictures and logos, links

--- altura/views.py
+++ altura/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
 def home(request):
     return render(request, 'altura/home.html')
+
+# Display EmpresaInfo
+def empresa(request):
+    return render(request, 'altura/empresa.html')
--- soltec/urls.py
+++ soltec/urls.py
@@ -18,8 +18,9 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', include('altura.urls')),
+
     path('admin/', admin.site.urls),
+    path('', include('altura.urls',)),
 ]
 
 # TODO: Might need removed..if not used.
